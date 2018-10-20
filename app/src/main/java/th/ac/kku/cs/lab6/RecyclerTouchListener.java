7ff2595d87ab76ae56637ea240352dc5057fe336
spidty <44290950+spidty@users.noreply.github.com>
2018-10-20 08:16:10 +0700

    test1

--- app/src/main/java/th/ac/kku/cs/lab6/RecyclerTouchListener.java
+++ app/src/main/java/th/ac/kku/cs/lab6/RecyclerTouchListener.java
@@ -1,4 +1,62 @@
 package th.ac.kku.cs.lab6;
 
-public class RecyclerTouchListener {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class RecyclerTouchListener implements RecyclerView.OnClickListener {
+    private ClickListener clickListener;
+    private GestureDetector gestureDetector;
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public RecyclerTouchListener(Context context,
+                                 final RecyclerView recyclerView,
+                                 final ClickListener clickListener) {
+        this.clickListener = clickListener;
+        gestureDetector = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapUp(MotionEvent e) {
+                        return true;
+                    }
+
+
+                    public void OnLongPress(MotionEvent e) {
+                        View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                        if (child != null && clickListener != null) {
+                            clickListener.onLongClick(child,
+                                    recyclerView.getChildAdapterPosition(child));
+                        }
+                    }
+                });
+    }
+
+    public boolean onInterceptTouchEvent(@NonNull RecyclerView recyclerView,
+                                         @NonNull MotionEvent motionEvent) {
+        View child = recyclerView.findChildViewUnder(motionEvent.getX(),
+                motionEvent.getY());
+        if (child != null && clickListener != null &&
+                gestureDetector.onTouchEvent(motionEvent)) {
+            clickListener.onClick(child,
+                    recyclerView.getChildAdapterPosition(child));
+
+        }
+
+        return false;
+
+
+    }
 }
--- app/src/main/java/th/ac/kku/cs/lab6/StudentAdapter.java
+++ app/src/main/java/th/ac/kku/cs/lab6/StudentAdapter.java
@@ -1,4 +1,58 @@
 package th.ac.kku.cs.lab6;
 
-public class StudentAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.MyViewHolder>{
+
+    private Context context;
+    private List<Student> studentListl;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView idTextView;
+        public TextView nameTextView;
+
+
+        public MyViewHolder(View view){
+            super(view);
+            idTextView = view.findViewById(R.id.text_view_id);
+            nameTextView = view.findViewById(R.id.text_view_name);
+        }
+    }
+
+    public StudentAdapter(Context context, List<Student> notesList) {
+        this.context = context;
+        this.studentListl = notesList;
+    }
+
+    @Nullable
+    @Override
+    public MyViewHolder onCreateViewHolder(@Nullable ViewGroup viewGroup, int i){
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.student_list_row, viewGroup, false);
+
+        return new MyViewHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder (@NonNull MyViewHolder myViewHolder, int position) {
+        Student student = studentListl.get(position);
+
+        myViewHolder.idTextView.setText(student.getId());
+        myViewHolder.nameTextView.setText(student.getName());
+    }
+
+    @Override
+    public  int getItemCount() {
+        return studentListl.size();
+    }
 }
